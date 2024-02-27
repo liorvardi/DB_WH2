9d007cacf84e758ca1108c6d44ebb38092082a00
liorvardi <lior1khtur@gmail.com>
2024-02-27 11:32:28 +0200

    implemented createTables

--- Solution.py
+++ Solution.py
@@ -18,13 +18,86 @@ from Business.Apartment import Apartment
 def create_tables():
     conn = Connector.DBConnector()
     try:
-        # conn.execute("CREATE TABLE Owners(id INTEGER PRIMARY KEY, name TEXT NOT NULL)")
-        # conn.execute("CREATE TABLE Apartments(id INTEGER PRIMARY KEY, owner_id INTEGER NOT NULL, name TEXT NOT NULL, location TEXT NOT NULL, price FLOAT NOT NULL, PRIMARY KEY(id), FOREIGN KEY(owner_id) REFERENCES Owners(id) ON DELETE CASCADE)")
-        conn.execute("CREATE TABLE Customers(id INTEGER PRIMARY KEY, name TEXT NOT NULL)")
-        # conn.execute("CREATE TABLE Reservations(customer_id INTEGER NOT NULL, apartment_id INTEGER NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, total_price FLOAT NOT NULL, PRIMARY KEY(customer_id, apartment_id, start_date), FOREIGN KEY(customer_id) REFERENCES Customers(id) ON DELETE CASCADE, FOREIGN KEY(apartment_id) REFERENCES Apartments(id) ON DELETE CASCADE)")
-        # conn.execute("CREATE TABLE Reviews(customer_id INTEGER NOT NULL, apartment_id INTEGER NOT NULL, review_date DATE NOT NULL, rating INTEGER NOT NULL, review_text TEXT NOT NULL, PRIMARY KEY(customer_id, apartment_id, review_date), FOREIGN KEY(customer_id) REFERENCES Customers(id) ON DELETE CASCADE, FOREIGN KEY(apartment_id) REFERENCES Apartments(id) ON DELETE CASCADE)")
+        queries = [
+            """
+            CREATE TABLE Owners(
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL
+            )
+            """,            
+            """
+            CREATE TABLE Customers(
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE Apartments(
+                id INTEGER PRIMARY KEY,
+                address TEXT NOT NULL,
+                city TEXT NOT NULL,
+                country TEXT NOT NULL,
+                size int NOT NULL,
+                check(size > 0),
+                CONSTRAINT unique_apartment UNIQUE(address, city, country)
+            )
+            """,
+            """
+            CREATE TABLE Reservations(
+                customer_id INTEGER NOT NULL,
+                apartment_id INTEGER NOT NULL,
+                start_date DATE NOT NULL,
+                end_date DATE NOT NULL,
+                total_price FLOAT NOT NULL,
+                PRIMARY KEY(customer_id, apartment_id, start_date),
+                FOREIGN KEY(customer_id) REFERENCES Customers(id) ON DELETE CASCADE,
+                FOREIGN KEY(apartment_id) REFERENCES Apartments(id) ON DELETE CASCADE,
+                check(start_date < end_date),
+                check(total_price > 0)
+            )
+            """,
+            """
+            CREATE TABLE OwnsApartment(
+                owner_id INTEGER NOT NULL,
+                apartment_id INTEGER NOT NULL,
+                PRIMARY KEY(apartment_id),
+                FOREIGN KEY(owner_id) REFERENCES Owners(id) ON DELETE CASCADE,
+                FOREIGN KEY(apartment_id) REFERENCES Apartments(id) ON DELETE CASCADE
+            )
+            """,
+            """
+            CREATE TABLE Reviews(
+                customer_id INTEGER NOT NULL,
+                apartment_id INTEGER NOT NULL,
+                review_date DATE NOT NULL,
+                rating INTEGER NOT NULL,
+                review_text TEXT NOT NULL,
+                PRIMARY KEY(customer_id, apartment_id),
+                FOREIGN KEY(customer_id) REFERENCES Customers(id) ON DELETE CASCADE,
+                FOREIGN KEY(apartment_id) REFERENCES Apartments(id) ON DELETE CASCADE,
+                check(rating >= 1 and rating <= 10)
+            )
+            """
+            """
+            CREATE VIEW ApartmentRating AS
+            SELECT apartment_id, AVG(rating) AS rating
+            FROM Reviews
+            GROUP BY apartment_id
+            """,       
+            """
+            CREATE VIEW OwnerRating AS
+            SELECT owner_id, AVG(rating) AS rating
+            FROM Reviews
+            JOIN OwnsApartment ON Reviews.apartment_id = OwnsApartment.apartment_id
+            GROUP BY owner_id
+            """, 
+            ]
+        for query in queries:
+            conn.execute(query)
+
     except Exception as e:
         print(e)
+        conn.rollback()
         return ReturnValue.ERROR
     finally:
         conn.close()
@@ -58,9 +131,29 @@ def delete_owner(owner_id: int) -> ReturnValue:
 
 
 def add_apartment(apartment: Apartment) -> ReturnValue:
-    # TODO: implement
-    pass
+    if(apartment.get_id() is None or apartment.get_id() <= 0
+       or apartment.get_address is None or ): return ReturnValue.BAD_PARAMS
+    conn = Connector.DBConnector()
+    try:
+        query = sql.SQL("INSERT INTO Apartments(id, owner_id, name, location, price) VALUES({id}, {addr}, {city}, {county}, {size})").format(
+            id=sql.Literal(apartment.get_id()),
+            addr=sql.Literal(apartment.get_address()),
+            city=sql.Literal(apartment.get_city()),
+            country=sql.Literal(apartment.get_country()),
+            size=sql.Literal(apartment.get_size())
+        )
+        rows_effected, _ = conn.execute(query)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        print(e)
+        return ReturnValue.ALREADY_EXISTS
+    except Exception  as e:
+        print(e)
+        conn.rollback()
+        return ReturnValue.ERROR
+    finally:
+        conn.close()
 
+    return ReturnValue.OK
 
 def get_apartment(apartment_id: int) -> Apartment:
     # TODO: implement
@@ -87,7 +180,9 @@ def add_customer(customer: Customer) -> ReturnValue:
         return ReturnValue.ALREADY_EXISTS
     except Exception  as e: 
         print(e)
+        conn.rollback()
         return ReturnValue.ERROR
+    
     finally:
         conn.close()
 
@@ -109,7 +204,10 @@ def get_customer(customer_id: int) -> Customer:
         return Customer(customer_id= resultSet.rows[0][0], customer_name= resultSet.rows[0][1])
     except Exception as e:
         print(e)
+        conn.rollback()
         return Customer.bad_customer()
+    finally:
+        conn.close()
 
 def delete_customer(customer_id: int) -> ReturnValue:
     if(customer_id is None or customer_id <= 0): return ReturnValue.BAD_PARAMS
@@ -121,6 +219,7 @@ def delete_customer(customer_id: int) -> ReturnValue:
             return ReturnValue.NOT_EXISTS
     except Exception as e:
         print(e)
+        conn.rollback()
         return ReturnValue.ERROR
     finally:
         conn.close()
